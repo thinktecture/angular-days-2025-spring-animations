thinktecture-labs/animation-showcase

feat: setup workspace

projects/components/src/list-item/list-item.component.ts
@@ -1,0 +1,17 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListItemData } from './list-item-data.type';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRippleModule } from '@angular/material/core';
+
+@Component({
+  selector: 'sl-list-item',
+  standalone: true,
+  imports: [CommonModule, MatIconModule, MatRippleModule],
+  templateUrl: './list-item.component.html',
+  styleUrls: ['./list-item.component.scss'],
+})
+export class ListItemComponent {
+  @Input() model?: ListItemData;
+  @Output() close = new EventEmitter<string>();
+}

projects/components/src/model/conference.model.ts
@@ -1,0 +1,7 @@
+export interface Conference {
+  id: string;
+  title: string;
+  imageUrl?: string;
+  color?: string;
+  description: string;
+}

projects/components/src/model/message.model.ts
@@ -1,0 +1,5 @@
+export interface Message {
+  id: string;
+  author: string;
+  shorthand: string;
+}

projects/conferences/src/app/components/home/home.component.ts
@@ -1,0 +1,24 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardComponent, ListItemComponent, SearchBarComponent } from '@sl/components';
+import { RouterLink } from '@angular/router';
+import { DataService } from '../../data.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, SearchBarComponent, CardComponent, ListItemComponent, RouterLink],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HomeComponent {
+  private readonly dataService = inject(DataService);
+
+  conferences$ = this.dataService.conferences$;
+  messages$ = this.dataService.messages$;
+
+  public deleteMessage(messageId: string): void {
+    this.dataService.deleteMessage(messageId);
+  }
+}

projects/conferences/src/app/components/messages/messages.component.ts
@@ -1,0 +1,21 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListItemComponent } from '@sl/components';
+import { DataService } from '../../data.service';
+
+@Component({
+  selector: 'app-messages',
+  standalone: true,
+  imports: [CommonModule, ListItemComponent],
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MessagesComponent {
+  private readonly dataService = inject(DataService);
+  messages$ = this.dataService.messages$;
+
+  public deleteMessage(messageId: string): void {
+    this.dataService.deleteMessage(messageId);
+  }
+}

thinktecture/angular-days-2025-spring-animations

projects/components/src/list-item/list-item.component.ts
@@ -1,0 +1,17 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListItemData } from './list-item-data.type';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRippleModule } from '@angular/material/core';
+
+@Component({
+  selector: 'sl-list-item',
+  standalone: true,
+  imports: [CommonModule, MatIconModule, MatRippleModule],
+  templateUrl: './list-item.component.html',
+  styleUrls: ['./list-item.component.scss'],
+})
+export class ListItemComponent {
+  @Input() model?: ListItemData;
+  @Output() close = new EventEmitter<number>();
+}

projects/components/src/model/conference.model.ts
@@ -1,0 +1,7 @@
+export interface Conference {
+  id: number;
+  title: string;
+  imageUrl?: string;
+  color?: string;
+  description: string;
+}

projects/components/src/model/message.model.ts
@@ -1,0 +1,5 @@
+export interface Message {
+  id: number;
+  author: string;
+  shorthand: string;
+}

projects/conferences/src/app/components/home/home.component.ts
@@ -1,0 +1,24 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardComponent, ListItemComponent, SearchBarComponent } from '@sl/components';
+import { RouterLink } from '@angular/router';
+import { DataService } from '../../data.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, SearchBarComponent, CardComponent, ListItemComponent, RouterLink],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HomeComponent {
+  private readonly dataService = inject(DataService);
+
+  conferences$ = this.dataService.conferences$;
+  messages$ = this.dataService.messages$;
+
+  public deleteMessage(messageId: number): void {
+    this.dataService.deleteMessage(messageId);
+  }
+}

projects/conferences/src/app/components/messages/messages.component.ts
@@ -1,0 +1,21 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListItemComponent } from '@sl/components';
+import { DataService } from '../../data.service';
+
+@Component({
+  selector: 'app-messages',
+  standalone: true,
+  imports: [CommonModule, ListItemComponent],
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MessagesComponent {
+  private readonly dataService = inject(DataService);
+  messages$ = this.dataService.messages$;
+
+  public deleteMessage(messageId: number): void {
+    this.dataService.deleteMessage(messageId);
+  }
+}